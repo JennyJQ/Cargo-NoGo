258b81919242af6e5d4ad7f57e19eae5a9f30801
Priyansh Shah <41307626+priyanshshah42@users.noreply.github.com>
2019-01-27 12:02:35 -0500

    Add files via upload

---
+++ Cargo No-Go/Cargo No-Go/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Cargo_No_Go
+{
+    partial class Cargo_No_Go
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cargo_No_Go));
+            this.start_button = new System.Windows.Forms.Button();
+            this.Stolen_Timer = new System.Windows.Forms.Timer(this.components);
+            this.truckIDListBox = new System.Windows.Forms.ListBox();
+            this.IDTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(12)))), ((int)(((byte)(22)))));
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.Location = new System.Drawing.Point(383, 248);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(233, 66);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "Start Security Systems";
+            this.start_button.UseVisualStyleBackColor = false;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // Stolen_Timer
+            // 
+            this.Stolen_Timer.Interval = 30000;
+            this.Stolen_Timer.Tick += new System.EventHandler(this.Stolen_Timer_Tick);
+            // 
+            // truckIDListBox
+            // 
+            this.truckIDListBox.FormattingEnabled = true;
+            this.truckIDListBox.Location = new System.Drawing.Point(32, 44);
+            this.truckIDListBox.Name = "truckIDListBox";
+            this.truckIDListBox.Size = new System.Drawing.Size(156, 238);
+            this.truckIDListBox.TabIndex = 1;
+            // 
+            // IDTextBox
+            // 
+            this.IDTextBox.Location = new System.Drawing.Point(32, 294);
+            this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.Size = new System.Drawing.Size(156, 20);
+            this.IDTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Active Device Serial Numbers:";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(32, 320);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(113, 320);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Cargo_No_Go.Properties.Resources.cargo_no_go2;
+            this.pictureBox1.Location = new System.Drawing.Point(309, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 74);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Cargo_No_Go
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(221)))), ((int)(((byte)(221)))));
+            this.ClientSize = new System.Drawing.Size(720, 383);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IDTextBox);
+            this.Controls.Add(this.truckIDListBox);
+            this.Controls.Add(this.start_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Cargo_No_Go";
+            this.Text = "Cargo No-Go";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Timer Stolen_Timer;
+        private System.Windows.Forms.ListBox truckIDListBox;
+        private System.Windows.Forms.TextBox IDTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Cargo No-Go/Cargo No-Go/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cargo_No_Go
+{
+    public partial class Cargo_No_Go : Form
+    {
+        public Cargo_No_Go()
+        {
+            InitializeComponent();
+            Stolen_Timer.Interval = 3000;
+        }
+
+        private List<string> id = new List<string>();
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (start_button.Text.Contains("Start"))
+            {
+                Stolen_Timer.Start();
+                start_button.Text = "Stop Security Systems";
+                addButton.Enabled = false;
+                removeButton.Enabled = false;
+            }
+            else
+            {
+                Stolen_Timer.Stop();
+                start_button.Text = "Start Security Systems";
+                addButton.Enabled = true;
+                removeButton.Enabled = true;
+            }
+        }
+        
+        private void Stolen_Timer_Tick(object sender, EventArgs e)
+        {
+            string[] dataFile = File.ReadAllLines(@"C:\Users\priya\Desktop\Cargo Theft Data Set\DemoData.csv");
+            List<string> dataList = dataFile.ToList<string>();
+            List<string> truckList = new List<string>();
+
+            foreach (string line in dataList)
+            {
+                string[] linearr = line.Split(',');
+                truckList.Add(linearr[1]);
+            }
+            
+            List<int> lastOccurrenceList = new List<int>();
+
+            foreach(string truck in id)
+                lastOccurrenceList.Add(truckList.LastIndexOf(truck));
+
+            for (int i = 0; i < lastOccurrenceList.Count; i++)
+            {
+                DateTime dateTime = DateTime.Parse(dataList[lastOccurrenceList[i]].Split(',').ElementAt(7));
+
+                if (Math.Abs(float.Parse(dataList[lastOccurrenceList[i]].Split(',').ElementAt(8)) - float.Parse(dataList[lastOccurrenceList[i]].Split(',').ElementAt(15))) > 0.009)
+                    MessageBox.Show($"Truck with device serial number {id[i]} has been stolen!", "Theft Reported", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else if(Math.Abs(float.Parse(dataList[lastOccurrenceList[i]].Split(',').ElementAt(9)) - float.Parse(dataList[lastOccurrenceList[i]].Split(',').ElementAt(16))) > 0.009)
+                    MessageBox.Show($"Truck with device serial number {id[i]} has been stolen!", "Theft Reported", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else if (DateTime.Now.Ticks - dateTime.Ticks > 36010000000)
+                    MessageBox.Show($"Truck with device serial number {id[i]} has had its GPS sensor destroyed or is undergoing maintenance.", "Suspected Theft", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //else if(off route)
+                //{
+                //stolen
+                //}
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(IDTextBox.Text, out int ID))
+            {
+                truckIDListBox.Items.Add(IDTextBox.Text);
+                id.Add(IDTextBox.Text);
+            }
+            else MessageBox.Show("That is not a valid device serial number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            IDTextBox.Text = "";
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (truckIDListBox.SelectedItem != null)
+            {
+                truckIDListBox.Items.Remove(truckIDListBox.SelectedItem);
+                id.Remove(truckIDListBox.SelectedItem.ToString());
+            }
+            else MessageBox.Show("Select the device you wish to stop tracking.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
